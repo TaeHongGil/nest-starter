80f377d0ad9fb315daddecbeb7e63174d6f9b387
taehong <taehong@doubleugames.com>
2024-04-08 12:27:45 +0900

    fix: registerControllerAsync 비동기방식으로 수정

--- src/server/server.module.ts
+++ src/server/server.module.ts
@@ -1,17 +1,16 @@
 import { Module, OnModuleInit } from '@nestjs/common';
 import { CoreModule } from '@root/core/core.module';
-import { ServerLogger } from '@root/core/server-log/server.log.service';
 import path from 'path';
 import { AccountModule } from './service/account/account.module';
 
 @Module({
-  imports: [AccountModule, CoreModule.registerController(ServerModule, path.join(__dirname, 'controller'), '.controller')],
+  imports: [AccountModule],
   controllers: [],
   providers: [],
   exports: [],
 })
 export class ServerModule implements OnModuleInit {
   async onModuleInit(): Promise<void> {
-    ServerLogger.log(`ServerModule.OnModuleInit`);
+    await CoreModule.registerControllerAsync(ServerModule, path.join(__dirname, 'controller'), '.controller');
   }
 }
