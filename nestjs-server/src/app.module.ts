a8bac010a34d0f71709fd394a54b5a3da24fa527
taehong <taehong@doubleugames.com>
2025-04-21 12:36:12 +0900

    feat: Throttler 모듈 추가 및 앱 모듈에 적용

--- nestjs-server/src/app.module.ts
+++ nestjs-server/src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
@@ -6,8 +8,35 @@ import { FeatureModule } from './feature/feature.module';
 import { ServerModule } from './server/server.module';
 
 @Module({
-  imports: [CoreModule, ServerModule, FeatureModule],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 30,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 300,
+      },
+    ]),
+    CoreModule,
+    ServerModule,
+    FeatureModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
