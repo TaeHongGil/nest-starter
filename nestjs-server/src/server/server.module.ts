74a842a390503db634927609e4ccf7f7b679c0bf
taehong <taehong@doubleugames.com>
2025-05-27 16:17:47 +0900

    fix: api폴더명 \b제거

--- nestjs-server/src/server/server.module.ts
+++ nestjs-server/src/server/server.module.ts
@@ -4,7 +4,7 @@ import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import ServerConfig from '@root/core/config/server.config';
 import { AuthGuard } from '@root/core/guard/auth.guard';
 import ServerLogger from '@root/core/server-log/server.logger';
-import { ApiModule } from './\bapi/api.module';
+import { ApiModule } from './api/api.module';
 import { MQConsumerModule } from './mq/consumer/mq.consumer.module';
 import { MQPublisherModule } from './mq/publisher/mq.publisher.module';
 import { WsModule } from './ws/ws.module';
